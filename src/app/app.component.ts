1614a0bd059f442236f5708a2a56bad1216a69fe
ayoub <ayoub.belghar@gmail.com>
2017-04-24 14:54:44 +0100

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,37 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'pomodoro-timer',
+  template: `
+  <h1>{{minutes}} : {{seconds}}</h1>
+  <p>
+    <button (click)="togglePause()">
+      {{ buttonLabel }}
+    </button>
+  </p>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  minutes: number;
+  seconds: number;
+
+  constructor() {
+    this.minutes = 24;
+    this.seconds = 59;
+    setInterval( () =>  this.tick(), 1000);
+  }
+
+  resetPomodoro() : void {
+    this.minutes = 24;
+    this.seconds = 59;
+  }
+
+private tick(): void {
+    if (--this.seconds < 0) {
+      this.seconds = 59;
+      if (--this.minutes < 0 ) {
+        this.resetPomodoro();
+      }
+    }
+  }
+}
--- src/app/dist/app/app.component.js
+++ src/app/dist/app/app.component.js
@@ -5,18 +5,37 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        var _this = this;
+        this.minutes = 24;
+        this.seconds = 59;
+        setInterval(function () { return _this.tick(); }, 1000);
     }
+    AppComponent.prototype.resetPomodoro = function () {
+        this.minutes = 24;
+        this.seconds = 59;
+    };
+    AppComponent.prototype.tick = function () {
+        if (--this.seconds < 0) {
+            this.seconds = 59;
+            if (--this.minutes < 0) {
+                this.resetPomodoro();
+            }
+        }
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
-        selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
-    })
+        selector: 'pomodoro-timer',
+        template: "\n  <h1>{{minutes}} : {{seconds}}</h1>\n  <p>\n    <button (click)=\"togglePause()\">\n      {{ buttonLabel }}\n    </button>\n  </p>\n  ",
+    }),
+    __metadata("design:paramtypes", [])
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
